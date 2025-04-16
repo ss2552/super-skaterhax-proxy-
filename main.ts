574ae53a9964d8af13c2c1885395a607315c08fe
ss2552 <85072753+ss2552@users.noreply.github.com>
2025-04-16 13:30:58 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,33 @@
+async function forward_proxy(url: URL, headers: Header, method: string) -> any{
+
+  // super-skaterhax の arm11code.bin からのリクエストか
+  if(
+    
+    method !== "GET" ||
+    url !== "https://smea.mtheall.com/get_ropbin_payload.php" ||
+    headers.get("User-Agent") !== "3dsbrowserhax_hblauncher_loader/v1.2"
+    
+  ){
+
+    return new Response("Not Found.", {status: 404});
+  }
+
+
+  //
+  const request_parameter = get_otherapp_bin_request_param(url.param);
+    
+  // 
+  const otherapp_bin = await fetch("http://smealum.github.io/"+request_parameter).then(
+    async res => await res.body();
+
+  return new Response(otherapp_bin, {headers: {"content-type": ""}});
+  
+}
+
+function get_otherapp_bin_request_param(parameter: Object) -> string{
+
+
+  
+  // https://smealum.github.io/ninjhax2/JL1Xf2KFVm/otherapp/POST5_J_12288_8203.bin
+  return
+}
